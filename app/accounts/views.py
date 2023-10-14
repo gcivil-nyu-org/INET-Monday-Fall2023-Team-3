5e66312ceffa6c850fba03c2445174e640d08c25
SoyMark <2575398569@qq.com>
2023-10-14 15:29:42 -0400

    useless

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,9 +1,22 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.urls import reverse_lazy
 from django.views import generic
 
 
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = UserCreationForm.Meta.fields
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("Username already exists")
+        return username
+
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
