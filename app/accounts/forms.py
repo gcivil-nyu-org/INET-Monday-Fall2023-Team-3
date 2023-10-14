c58c1af08044caabc341c512fefc84ecbee1b509
Laylaiii <46698580+IreneLXR@users.noreply.github.com>
2023-10-14 15:25:28 -0400

    add validation for username and email address

---
+++ app/accounts/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+import re
+
+class RegisterForm(UserCreationForm):
+    
+    class Meta:
+        model=User
+        fields = ['username','email','password1','password2'] 
+
+    
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("Username is already registered")
+        return username
+    
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        # check exist email
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email is already registered")
+        # validate email
+        email_regex = re.compile(r'^[a-zA-Z0-9_.+-]+@nyu\.edu$')
+        if bool(email_regex.match(email)) == False:
+            raise forms.ValidationError("Invalid Email Address: The provided email address is not associated with NYU. Please ensure you're using an email that ends with @nyu.edu.")
+        return email
+    
+
+    
--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-
+from .forms import RegisterForm
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = RegisterForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
