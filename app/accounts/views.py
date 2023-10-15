0cbc69314553ca9ff628683fe9e2e71cb2790e17
sudo <56063116+mkZH0740@users.noreply.github.com>
2023-10-14 22:47:20 -0400

    clean up code

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -6,16 +6,6 @@ from django.views import generic
 from .forms import RegisterForm
 from django.shortcuts import render, redirect
 
-class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = UserCreationForm.Meta.fields
-
-    def clean_username(self):
-        username = self.cleaned_data.get('username')
-        if User.objects.filter(username=username).exists():
-            raise ValidationError("Username already exists")
-        return username
 
 class SignUpView(generic.CreateView):
     form_class = RegisterForm
