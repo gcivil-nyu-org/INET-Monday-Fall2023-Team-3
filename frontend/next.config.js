3c5c0d84c638e80a32c94d817c09d242ba425539
sudo <texasandlappland@gmail.com>
2023-10-20 00:56:49 -0400

    Add Nextjs proxy to backend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [{
+      source: "/backend/user/login",
+      destination: "http://backend:8000/user/login/",
+    }, {
+      source: "/backend/user/signup",
+      destination: "http://backend:8000/user/signup/",
+    }, {
+      source: "/backend/user/update",
+      destination: "http://backend:8000/user/update/"
+    }]
+  }
+}
 
 module.exports = nextConfig
