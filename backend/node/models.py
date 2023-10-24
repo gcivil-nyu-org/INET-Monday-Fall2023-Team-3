75a6e52a1a3414e13bd474acf132996bdd86b67f
Laylaiii <46698580+IreneLXR@users.noreply.github.com>
2023-10-24 18:21:43 -0400

    reformat and ensure form needed to be filled before submission

--- backend/node/models.py
+++ backend/node/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 
 # Create your models here.
-class Node(models.Model): 
+class Node(models.Model):
     name = models.CharField(max_length=60)
     description = models.TextField(blank=True)
-    
 
     def __str__(self):
         return self.name
