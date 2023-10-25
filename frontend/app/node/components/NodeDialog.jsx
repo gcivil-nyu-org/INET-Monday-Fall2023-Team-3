9c5d3d0bda6c40ab83f13cb44c016ee606399ca0
Laylaiii <46698580+IreneLXR@users.noreply.github.com>
2023-10-24 21:08:26 -0400

    update dialog layout

--- frontend/app/node/components/NodeDialog.jsx
+++ frontend/app/node/components/NodeDialog.jsx
@@ -1,47 +1,50 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import { Button, Alert, TextField } from "@mui/material"
 
-function NodeDialog({ showDialog, onSubmit, onClose }) {
-    const [formData, setFormData] = useState({
-        courseName: '',
-        description: ''
-    })
+function NodeDialog({ showDialog, onClose }) {
 
-    const handleChange = (event) => {
-        const {name, value} = event.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
+    const [name, setName] = useState("")
+    const [description, setDescription] = useState("")
+  
+    const onAddButtonClicked = () => {
+      console.log(`name: ${name}`)
+      console.log(`description: ${description}`)
+      nodeCreate({
+        name: name,
+        description: description,
+      }).then((result) => {
+        if (result.status) {
+          setErrorMessage("")
+          console.log(result.value)
+        } else {
+          setErrorMessage(result.error)
+          console.error(result.error)
+        }
+      })
+    }
 
-    const handleSubmit = () => {
-        console.log(formData);
-        onSubmit(formData);
-        setFormData('');
-    };
+    const onNameInputChanged = (event: ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value)
+    }
+  
+    const onDescriptionInputChanged = (event: ChangeEvent<HTMLInputElement>) => {
+        setDescription(event.target.value)
+    }
 
     return (
         showDialog && (
-            <div style={{border: '1px solid black', padding: '10px', marginTop: '20px'}}>
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Course Name:
-                        <input type="text" name="courseName" value={formData.courseName} onChange={handleChange} required/>
-                    </label>
-                    <label>
-                        Description:
-                        <textarea 
-                            placeholder="Enter Course Description" 
-                            value={formData.description}
-                            onChange={handleChange}
-                            required 
-                        />
-                    </label>
-                    <br />
-                    <button type="submit">Submit</button>
-                    <button onClick={onClose}>Cancel</button>
-                </form>
+            <div className="w-full flex flex-col items-center bg-white">
+            <div className="h-24 w-full flex">
+              <TextField className="h-16 m-4 flex-1" label="name" variant="outlined" onChange={onNameInputChanged} />
             </div>
+            <div className="h-24 w-full flex">
+              <TextField className="h-16 m-4 flex-1" label="description" variant="outlined" onChange={onDescriptionInputChanged} />
+            </div>
+            <div className="h-24 w-full flex">
+              <Button className="w-64 h-16 m-auto bg-blue-400" size="large" variant="contained" onClick={onAddButtonClicked}>Add</Button>
+              <Button className="w-64 h-16 m-auto bg-blue-400" size="large" variant="contained" onClick={onClose}>Close</Button>
+            </div>
+          </div>
         )
     );
 }
