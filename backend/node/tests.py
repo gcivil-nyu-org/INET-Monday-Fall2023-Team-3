b4d7d24fcd2525385cddcf67c044a01c19b7c229
Laylaiii <46698580+IreneLXR@users.noreply.github.com>
2023-10-29 03:28:23 -0400

    add tests to user and update tests for node

--- backend/node/tests.py
+++ backend/node/tests.py
@@ -1,10 +1,16 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 from .models import Node
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 
 class NodeTests(APITestCase):
     def setUp(self):
+        self.test_user = User.objects.create_user(
+            "testuser", "test@example.com", "testpassword"
+        )
+        self.test_user_token = Token.objects.create(user=self.test_user)
         # Set up any objects you need for the tests
         self.predefined_nodes_count = Node.objects.filter(isPredefined=True).count()
         self.total_nodes_count = Node.objects.count()
@@ -41,6 +47,7 @@ class NodeTests(APITestCase):
 
     def test_node_create(self):
         # Test the node_create view
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.test_user_token.key)
         data = {"name": "New Node", "description": "A new node", "isPredefined": False}
         response = self.client.post("/node/create/", data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
