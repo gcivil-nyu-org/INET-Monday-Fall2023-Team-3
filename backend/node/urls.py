b0118a8b28dbee1f631e3de6f43e051021ba4ef2
Sahil Faizal <106440078+sahilfaizal01@users.noreply.github.com>
2023-10-30 22:30:03 -0300

    node-edit-url added to urls.py

--- backend/node/urls.py
+++ backend/node/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("ping/", views.ping, name="ping"),
     path("nodes/", views.node_list, name="node-list"),
     path("create/", views.node_create, name="node-create"),
+    path("edit/", views.node_edit, name="node-edit"),
     path("predefined-nodes/", views.predefined_node_list, name="predefined-node-list"),
 ]
