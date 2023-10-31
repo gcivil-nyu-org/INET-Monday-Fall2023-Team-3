13ed023cbd2d5d58836e3658200b62639d51cd93
sudo <texasandlappland@gmail.com>
2023-10-31 03:29:40 -0400

    feat(backend): add node model

--- backend/node/models.py
+++ backend/node/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+import uuid
+
 # Create your models here.
+
+
+class Node(models.Model):
+    id = models.UUIDField(
+        "id", primary_key=True, unique=True, editable=False, default=uuid.uuid4
+    )
+    name = models.CharField(max_length=60)
+    description = models.TextField(blank=True)
+    predefined = models.BooleanField(default=False)
+    dependencies = models.ManyToManyField("self", symmetrical=False, blank=True)
+
+    def __str__(self) -> str:
+        return f"Node {self.name}: id={self.id} is_predefined={self.predefined}"
