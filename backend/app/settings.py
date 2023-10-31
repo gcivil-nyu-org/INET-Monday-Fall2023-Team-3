a1803bcd1907bdeb0eb8130e3b09be6ab518f35e
sudo <texasandlappland@gmail.com>
2023-10-31 18:35:25 -0400

    fix(backend): fix allowed host format

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    "http://smooth-dirty.us-west-2.elasticbeanstalk.com/",
+    "smooth-dirty.us-west-2.elasticbeanstalk.com",
 ]
 
 
