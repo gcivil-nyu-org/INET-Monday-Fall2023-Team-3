a80ddc88dbfeac1c2bbf9f2eaa93a43c1e0c146c
sudo <texasandlappland@gmail.com>
2023-10-31 18:47:02 -0400

    feat(backend): fix compiled file not found

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -132,6 +132,9 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    BASE_DIR / "static" / "static",
+    BASE_DIR / "static" / "static" / "js",
+    BASE_DIR / "static" / "static" / "css",
 ]
 
 # Default primary key field type
