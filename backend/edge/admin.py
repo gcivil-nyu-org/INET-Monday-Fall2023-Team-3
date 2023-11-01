c4217014493a6facd208fc5c45dc1e3f9d98effd
sudo <texasandlappland@gmail.com>
2023-11-01 00:34:05 -0400

    fix(other): remove merge leftover

--- backend/edge/admin.py
+++ backend/edge/admin.py
@@ -3,11 +3,5 @@ from django.contrib import admin
 from .models import Edge
 
 # Register your models here.
-<<<<<<<< HEAD:backend/edge/admin.py
 
 admin.site.register(Edge)
-========
-from .models import Node
-
-admin.site.register(Node)
->>>>>>>> develop:backend/node/admin.py
--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-  beanstalk-deploy "$AWS_APP_NAME" "$AWS_APP_ENV_PROD" "$EB_DEPLOY_VERSION" "$AWS_DEPLOY_REGION" deploy.zip
-elif [ "$TRAVIS_BRANCH" = "develop" ]; then
-  beanstalk-deploy "$AWS_APP_NAME" "$AWS_APP_ENV_DEV" "$EB_DEPLOY_VERSION" "$AWS_DEPLOY_REGION" deploy.zip
-else
-  beanstalk-deploy "$AWS_APP_NAME" "$AWS_APP_ENV_DIRTY" "$EB_DEPLOY_VERSION" "$AWS_DEPLOY_REGION" deploy.zip
+if [ "$TRAVIS_PULL_REQUEST" = "false"]; then
+  if [ "$TRAVIS_BRANCH" = "master" ]; then
+    beanstalk-deploy "$AWS_APP_NAME" "$AWS_APP_ENV_PROD" "$EB_DEPLOY_VERSION" "$AWS_DEPLOY_REGION" deploy.zip
+  elif [ "$TRAVIS_BRANCH" = "develop" ]; then
+    beanstalk-deploy "$AWS_APP_NAME" "$AWS_APP_ENV_DEV" "$EB_DEPLOY_VERSION" "$AWS_DEPLOY_REGION" deploy.zip
+  else
+    beanstalk-deploy "$AWS_APP_NAME" "$AWS_APP_ENV_DIRTY" "$EB_DEPLOY_VERSION" "$AWS_DEPLOY_REGION" deploy.zip
+  fi
 fi
