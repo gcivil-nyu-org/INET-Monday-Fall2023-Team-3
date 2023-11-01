88eac82a24a9b1f38d8c547408f1e8f4c76f68e7
sudo <texasandlappland@gmail.com>
2023-11-01 00:39:35 -0400

    fix(other): fix deploy script syntax

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "$TRAVIS_PULL_REQUEST" = "false"]; then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   if [ "$TRAVIS_BRANCH" = "master" ]; then
     beanstalk-deploy "$AWS_APP_NAME" "$AWS_APP_ENV_PROD" "$EB_DEPLOY_VERSION" "$AWS_DEPLOY_REGION" deploy.zip
   elif [ "$TRAVIS_BRANCH" = "develop" ]; then
