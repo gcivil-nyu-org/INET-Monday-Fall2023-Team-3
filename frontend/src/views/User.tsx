4bce10049af0b9ec42acd31975da4fcb68c3b5aa
SoyMark <2575398569@qq.com>
2023-11-03 13:53:23 -0400

    create graph when clicking  botton

--- frontend/src/views/User.tsx
+++ frontend/src/views/User.tsx
@@ -3,17 +3,38 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import GraphList from "components/user/GraphList";
 import Update from "components/user/Update";
+import { graphCreate, userGet } from "utils/backendRequests";
 
 export default function User() {
   const navigate = useNavigate();
   const [update, setUpdate] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
 
   const onUpdateCancelled = () => {
     console.log("update cancelled");
     setUpdate(false);
   };
-
+  // get current user email
   const onCreateGraphButtonClicked = () => {
+    userGet(sessionStorage.getItem("token")!).then((result) => {
+      if (result.status) {
+        const user = result.value;
+        setUserEmail(user.email);
+      } else {
+        console.log("Cannot get current user")
+      }
+    });
+    // create graph
+    graphCreate(
+      {user: userEmail, editingEnabled: true},
+      sessionStorage.getItem("token")!).then((result) => {
+        if (result.status) {
+          sessionStorage.setItem("graphId", result.value.id);
+        } else {
+          console.log("Cannot create graph")
+        }
+      });
+
     navigate("/graph");
   };
 
