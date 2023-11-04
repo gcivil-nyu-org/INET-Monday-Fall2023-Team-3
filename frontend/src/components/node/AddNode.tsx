8723a387a7b20fe01662427667d6a757a58a821a
Laylaiii <46698580+IreneLXR@users.noreply.github.com>
2023-11-04 14:13:43 -0400

    update layout

--- frontend/src/components/node/AddNode.tsx
+++ frontend/src/components/node/AddNode.tsx
@@ -1,8 +1,10 @@
 import { useState, ChangeEvent } from "react";
-import { Button, TextField, Alert } from "@mui/material";
-
+import { Button, TextField, Alert, Typography } from "@mui/material";
 import { INode } from "utils/models";
 import { nodeCreate } from "utils/backendRequests";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Theme from "./Theme"; // Import your theme configuration
 
 export type AddNodeProp = {
   predefinedNodes: INode[];
@@ -63,77 +65,86 @@ export default function AddNode({
   };
 
   return (
-    <div className="w-full flex">
-      <div className="w-1/3 flex flex-col items-center bg-white">
-        <div className="h-24 w-full flex">
-          <TextField
-            className="h-16 m-4 flex-1"
-            label="Search"
-            variant="outlined"
-            fullWidth
-            onChange={onSearchChanged}
-          />
-        </div>
-        <div className="w-full flex flex-col items-center bg-white overflow-y-auto max-h-[19rem] p-4">
-          <div className="grid grid-cols-1 md:grid-cols-1 gap-4 w-full">
-            {filteredNodes.map((node) => {
-              if (onCanvasNodeIds.includes(node.id)) {
-                return null; // Skip rendering if node's ID is in nodeIds
-              }
-              return (
-                <Button
-                  className="flex items-center justify-center min-h-[6rem] min-w-[8rem] bg-gray-200 rounded-md p-4 text-center overflow-hidden"
-                  key={node.name}
-                  onClick={() => onAddButtonClicked(node)}
-                >
-                  <span className="whitespace-normal text-sm">{node.name}</span>
-                </Button>
-              );
-            })}
+    <ThemeProvider theme={Theme}>
+      <CssBaseline />
+      <div className="w-full flex">
+        <div className="w-1/3 flex flex-col items-center bg-gray-200" style={{ flexGrow: 1 }}>
+          <div className="h-24 w-full flex">
+            <TextField
+              className="h-16 m-4 flex-1"
+              label="Search"
+              variant="outlined"
+              fullWidth
+              onChange={onSearchChanged}
+            />
           </div>
-        </div>
-      </div>
-
-      {/* AddNode Component */}
-      <div className="w-2/3 flex flex-col items-center bg-white">
-        <div className="my-4 w-full flex">
-          <TextField
-            className="m-4 flex-1"
-            label="Course Name"
-            variant="outlined"
-            onChange={onNameInputChanged}
-            required
-          />
-        </div>
-        <div className="w-full flex my-4">
-          <TextField
-            className="flex-1 mx-4 min-h-[6rem]"
-            label="Description"
-            variant="outlined"
-            onChange={onDescriptionInputChanged}
-            required
-            multiline
-            rows={4}
-          />
-        </div>
-        {errorMessage !== "" && (
-          <div className="w-full my-4">
-            <Alert className="h-16 w-full m-4" severity="error">
-              {errorMessage}
-            </Alert>
-          </div>
-        )}
-        <div className="w-full flex my-4">
-          <Button
-            className="w-64 h-16 m-auto bg-blue-400 my-4"
-            size="large"
-            variant="contained"
-            onClick={onSave}
+          <div
+            className="w-full flex flex-col items-center bg-gray-200 overflow-y-auto max-h-[20rem]"
+            style={{ flexGrow: 1 }}
           >
-            Save
-          </Button>
+            <div className="grid grid-cols-1 md:grid-cols-1 gap-4 w-full">
+              {filteredNodes.map((node) => {
+                if (onCanvasNodeIds.includes(node.id)) {
+                  return null; // Skip rendering if node's ID is in nodeIds
+                }
+                return (
+                  <Button
+                    className="flex items-center justify-center min-h-[6rem] min-w-[6rem] bg-white rounded-md p-4 text-center overflow-hidden mx-2"
+                    key={node.name}
+                    onClick={() => onAddButtonClicked(node)}
+                  >
+                    <span className="whitespace-normal text-sm">{node.name}</span>
+                  </Button>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+
+        {/* AddNode Component */}
+        <div className="w-2/3 flex flex-col items-center bg-white" style={{ flexGrow: 1 }}>
+          <Typography variant="h6" component="h3" className="w-full text-center my-4">
+            Add Node
+          </Typography>
+          <div className="my-4 w-full flex">
+            <TextField
+              className="m-4 flex-1"
+              label="Course Name"
+              variant="outlined"
+              onChange={onNameInputChanged}
+              required
+            />
+          </div>
+          <div className="w-full flex my-4">
+            <TextField
+              className="flex-1 mx-4 min-h-[6rem]"
+              label="Description"
+              variant="outlined"
+              onChange={onDescriptionInputChanged}
+              required
+              multiline
+              rows={4}
+            />
+          </div>
+          {errorMessage !== "" && (
+            <div className="w-full my-4">
+              <Alert className="h-16 w-full m-4" severity="error">
+                {errorMessage}
+              </Alert>
+            </div>
+          )}
+          <div className="w-full flex my-4">
+            <Button
+              className="w-64 h-16 m-auto bg-blue-400 my-4"
+              size="large"
+              variant="contained"
+              onClick={onSave}
+            >
+              Save
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
