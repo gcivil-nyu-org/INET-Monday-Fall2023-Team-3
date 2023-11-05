54e1f2ce9c9a893441b407423e6ff4f90d5de903
siyan liu <siyanliuads@gmail.com>
2023-11-04 19:43:54 -0500

    confirm merge

--- backend/graph/admin.py
+++ backend/graph/admin.py
@@ -3,16 +3,4 @@ from graph.models import Graph
 
 # Register your models here.
 
-<<<<<<< HEAD
 admin.site.register(Graph)
-=======
-from node.admin import NodeInline
-
-admin.site.register(Graph)
-
-# this is for testing purpose.
-# Allows you to see the nodes added to a certain graph in /admin
-# @admin.register(Graph)
-# class GraphAdmin(admin.ModelAdmin):
-#     inlines = [NodeInline]
->>>>>>> 3c4db39054d93d3a88370b80a4f08c4abb1ee31d
--- backend/graph/models.py
+++ backend/graph/models.py
@@ -6,24 +6,14 @@ from node.models import Node
 from edge.models import Edge
 
 class Graph(models.Model):
-<<<<<<< HEAD
-    graph_id = models.UUIDField(
-        primary_key=True, unique=True, editable=False, default=uuid.uuid4
-=======
     id = models.UUIDField(
         "id", primary_key=True, unique=True, editable=False, default=uuid.uuid4
->>>>>>> 3c4db39054d93d3a88370b80a4f08c4abb1ee31d
     )
     editingEnabled = models.BooleanField(default=False)
 
     # foreign key in node.models already implicitly set the relationship here
-<<<<<<< HEAD
-    node_ids = models.JSONField(default=list)
-    edge_ids = models.JSONField(default=list)
-=======
     nodes = models.ManyToManyField(Node, blank=True)
     edges = models.ManyToManyField(Edge, blank=True)
->>>>>>> 3c4db39054d93d3a88370b80a4f08c4abb1ee31d
     user = models.ForeignKey(
         CustomUser, on_delete=models.CASCADE, related_name="graphs", null=True
     )
