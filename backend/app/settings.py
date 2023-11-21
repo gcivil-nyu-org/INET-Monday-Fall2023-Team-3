fca97749069aac38044217621f16a08af80c0641
mkZH0740 <texasandlappland@gmail.com>
2023-11-21 00:25:13 -0500

    refactor(backend): add backend endpoint prefix

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -121,3 +121,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Backend endpoint prefix
+BACKEND_URL_ENDPOINT: "backend/"
