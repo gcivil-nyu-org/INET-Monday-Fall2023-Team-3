148ba79338578a279f6242ea65f910edc3d68728
mkZH0740 <texasandlappland@gmail.com>
2023-11-20 23:25:13 -0500

    refactor: clean up

--- backend/comment/routing.py
+++
@@ -1,7 +0,0 @@
-# routing.py
-from django.urls import path
-from .consumers import CommentConsumer
-
-websocket_urlpatterns = [
-    path("ws/comment/", CommentConsumer.as_asgi()),
-]
--- backend/graph/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from graph.models import Graph
-
-# Register your models here.
-
-admin.site.register(Graph)
