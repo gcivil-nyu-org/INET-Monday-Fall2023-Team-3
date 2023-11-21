516f3dc6e2b806470553cab2389f4ef25fce9971
mkZH0740 <texasandlappland@gmail.com>
2023-11-21 15:42:26 -0500

    refactor(backend): fix node url path naming

--- backend/node/urls.py
+++ backend/node/urls.py
@@ -7,9 +7,9 @@ app_name = "node"
 urlpatterns = [
     path(views.NODE_PING_PATH, views.node_ping, name="ping"),
     path(views.NODE_CREATE_PATH, views.node_create, name="create"),
-    path(views.NODE_DELETE_PATH, views.node_delete, name="delete"),
-    path(views.NODE_PATCH_PATH, views.node_patch, name="update"),
     path(views.NODE_GET_PATH, views.node_get, name="get"),
+    path(views.NODE_PATCH_PATH, views.node_patch, name="patch"),
+    path(views.NODE_DELETE_PATH, views.node_delete, name="delete"),
     path(
         views.NODE_GET_PREDEFINED_PATH, views.node_get_predefined, name="get-predefined"
     ),
