126b7fce6b6f6c29c805fe90b2d8b301e66c22ba
mkZH0740 <texasandlappland@gmail.com>
2023-11-29 03:46:39 -0500

    refactor(frontend): fix frontend launch

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter, redirect } from "react-router-dom";
+import Welcome from "./views/Welcome";
+import User from "./views/User";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Welcome />,
+  },
+  {
+    path: "/user",
+    element: <User />,
+    loader: async () => {
+      const token = sessionStorage.getItem("token");
+      if (token === null) {
+        return redirect("/");
+      }
+      return null;
+    },
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
