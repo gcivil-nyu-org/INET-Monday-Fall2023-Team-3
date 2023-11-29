9d2e0e8f532bb4505a459a06086b90cf2535778d
mkZH0740 <texasandlappland@gmail.com>
2023-11-29 02:34:15 -0500

    refactor(frontend): add path resolving

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      src: "/src",
+      utils: "/src/utils",
+      components: "/src/components",
+      views: "/src/views",
+    },
+  },
+});
