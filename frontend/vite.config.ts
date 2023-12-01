49876d63fc4f52a503e577c0864f5ce9fb0476c5
mkZH0740 <texasandlappland@gmail.com>
2023-12-01 00:46:01 -0500

    refactor(frontend): use proxy when dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,29 @@
-import { defineConfig } from "vite";
+import { UserConfig, defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      src: "/src",
-      utils: "/src/utils",
-      components: "/src/components",
-      views: "/src/views",
+export default defineConfig(({ command }) => {
+  const baseConfig: UserConfig = {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        src: "/src",
+        utils: "/src/utils",
+        components: "/src/components",
+        views: "/src/views",
+      },
     },
-  },
+  };
+
+  if (command === "serve") {
+    baseConfig.server = {
+      proxy: {
+        "/backend/": {
+          target: "http://127.0.0.1:8000/",
+        },
+      },
+    };
+  }
+
+  return baseConfig;
 });
