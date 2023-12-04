e123ae6c9fe37d78b112fc69eebc74377cfeeb2d
siyan liu <siyanliuads@gmail.com>
2023-12-04 00:05:51 -0600

    welcome page styled

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      rotate: {
+        '20': '20deg',
+        '15': '15deg',
+      }
+    },
+    colors: {
+      'orange': '#F3AC42',
+      'yellow': '#FCF071',
+      'blue': '#90D5D9',
+      'pink': '#F2ADBE',
+      'green': '#9EDF76',
+      'beige': '#F9F4EB',
+      'olive': '#2B5413',
+    },
+    fontFamily: {
+      sans: ['Archivo Black', 'sans-serif'],
+      serif: ['Benne', 'serif'],
+    }
   },
   plugins: [],
 }
