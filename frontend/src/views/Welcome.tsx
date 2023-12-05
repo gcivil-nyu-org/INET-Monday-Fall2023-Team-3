01afa9fce6614ad0773d9d8c2cd6aa3d26a190fd
siyan liu <siyanliuads@gmail.com>
2023-12-04 22:36:50 -0600

    clean up

--- frontend/src/views/Welcome.tsx
+++ frontend/src/views/Welcome.tsx
@@ -25,67 +25,6 @@ export default function Welcome() {
   };
 
   return (
-    // <div className="w-full h-full flex flex-col min-h-screen items-center justify-between">
-    //   <div className="w-full flex h-24 flex-row bg-slate-100">
-    //     <div className="flex h-24 flex-row">
-    //       <div className="h-16 w-16 bg-slate-400 m-4"></div>
-    //       <div className="h-16 m-4">
-    //         <span className="h-16 flex items-center justify-center text-center m-auto text-lg">
-    //           SMOOTH
-    //         </span>
-    //       </div>
-    //     </div>
-
-    //     <div className="flex h-24 flex-row ml-auto">
-    //       <div className="flex h-16 m-4">
-    //         <Button
-    //           className="w-60 mr-4 bg-gray-500"
-    //           size="large"
-    //           variant="contained"
-    //           onClick={onSignUpButtonClicked}
-    //         >
-    //           Sign Up
-    //         </Button>
-    //         <Button
-    //           className="w-60 bg-blue-400"
-    //           size="large"
-    //           variant="contained"
-    //           onClick={onLoginButtonClicked}
-    //         >
-    //           Log In
-    //         </Button>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <div className="w-full flex flex-row self-stretch items-stretch justify-between flex-1">
-    //     <div className="flex flex-1 bg-gray-500"></div>
-    //     <Dialog open={login || signUp} onClose={onCancel} maxWidth="sm" fullWidth={true}>
-    //       {login && ( // if login is true, then render the Login component
-    //         <>
-    //           <DialogTitle>Log In</DialogTitle>
-    //           <Login />
-    //         </>
-    //       )}
-    //       {signUp && ( // if signup is true, then render the Signup component
-    //         <>
-    //           <DialogTitle>Sign Up</DialogTitle>
-    //           <SignUp />
-    //         </>
-    //       )}
-    //     </Dialog>
-    //   </div>
-    // </div>
-    // <div className="bg-beige w-full h-screen flex justify-center">
-    //   <div className="w-3/6 inline absolute left text-3xl font-bold">
-    //     SMOOTH IS A TASK AND DEPENDENCY
-    //     MANAGEMENT TOOL DESIGNED TO STREAMLINE
-    //     CURRICULUM PLANNING AT NYU. USERS CAN
-    //     CREATE NODES REPRESENTING TASKS OR COURSES
-    //     AND ESTABLISH DEPENDENCIES BETWEEN THEM BY DRAWING EDGES.
-    //   </div>
-    // </div>
-
-
     <div className="bg-beige w-full h-full overflow-hidden">
       <div className="bg-beige flex flex-row w-full">
         <div className="basis-1/2 m-5">
